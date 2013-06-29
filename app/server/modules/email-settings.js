4e743ea5b0ae0884f22f013714aacd44e6c56444
Devin Doolin <icddevin@gmail.com>
2013-06-29 15:00:15 +0000

    Typo fix for email password field

--- app/server/modules/email-settings.js
+++ app/server/modules/email-settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: "smtp.gmail.com",
     user: process.env["DISPATCH_EMAIL"],
-    password: ["DISPATCH_EMAIL_PASS"],
+    password: process.env["DISPATCH_EMAIL_PASS"],
     sender: "SeoulMix <contact@seoulmix.com>"
 };
