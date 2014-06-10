fb83b5f7661ee31ae5728d2ffc06ba11f97849f6
Devin Doolin <icddevin@gmail.com>
2014-06-10 10:10:55 -0400

    Leftover additions

--- app/server/modules/constants.js
+++ app/server/modules/constants.js
@@ -1,2 +1,2 @@
 // Handy place to store some application constants if nowhere more appropriate
-exports.DEFAULT_PROFILE_IMAGE_ID = process.env["DEFAULT_PROFILE_IMAGE_ID"];
+exports.DEFAULT_PROFILE_IMAGE_ID = process.env["DEFAULT_PROFILE_IMAGE_ID"] || 'default_profile_ofegjd';
